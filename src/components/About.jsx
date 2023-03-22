4b9364bb87a13cc49ddd1092739bcd0210970dbf
Thuan <nguyenmthuan18@gmail.com>
2023-03-22 10:46:58 +0700

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
             <h2>Delicious Food, Delivered to You</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloremque accusantium maxime hic odit dolorem.</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis minus doloribus perferendis.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis minus doloribus perferendis.</p>
         </div>
     )
 }
